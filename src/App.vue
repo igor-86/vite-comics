f9fb327b02188094b240bd50c98f0c2b59b347dd
igor-86 <i.campoli86@gmail.com>
2022-11-09 18:39:42 +0100

    Cards slide in Components Complete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-
+import AppBanner from './components/AppBanner.vue';
 export default {
   data() {
     return {
@@ -9,14 +9,24 @@ export default {
   },
   components: {
     AppHeader,
+    AppBanner,
   },
 }
 </script>
 
 <template>
   <AppHeader />
+  <section>
+    <AppBanner />
+  </section>
+
 </template>
 
 <style lang="scss">
 @use "./styles/general.scss" as*;
+@use "./styles/partials/variables" as*;
+
+section {
+  background-color: $color-current-slide;
+}
 </style>
---
+++ src/components/AppCards.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    name: "AppCards",
+    props: {
+        thumb: String,
+        series: String,
+        price: String
+    },
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="img-card">
+            <img :src="thumb" alt="series">
+
+        </div>
+        <h3>{{ series }}</h3>
+        <h4>{{ price }}</h4>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    width: calc(100% / 6 - 1rem);
+
+    .img-card {
+        width: 100%;
+        height: 180px
+    }
+
+    img {
+        object-fit: cover;
+        object-position: top;
+        height: 100%;
+        width: 100%;
+    }
+
+    h3,
+    h4 {
+        color: white;
+        font-weight: 500;
+        font-size: .8rem;
+        text-transform: uppercase;
+        margin-top: .5rem;
+    }
+}
+</style>
